9f75c333dc431f563cd33e63489b1b88befe53d4
RaviJM <ravi.mak2003@gmail.com>
2024-06-16 00:17:35 +0530

    add css to Scoreboard
    added css to Scoreboard and modified Card.css(font size) and App.css

--- memory-game/src/components/Scoreboard/Scoreboard.jsx
+++ memory-game/src/components/Scoreboard/Scoreboard.jsx
@@ -1,9 +1,13 @@
+import "../Scoreboard/Scoreboard.css";
+
 function Scoreboard({ currentScore, highScore }) {
   return (
     <div className="scoreboard-container">
       <h1>Scoreboard</h1>
-      <p>Score: {currentScore}</p>
-      <p>High Score: {highScore}</p>
+      <div className="scores">
+        <p>Score: {currentScore}</p>
+        <p>High Score: {highScore}</p>
+      </div>
     </div>
   );
 }
